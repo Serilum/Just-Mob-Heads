ca5fe0ee5519294f4b8ca1cf457f215d32594ed0
Rick South <rick@natamus.com>
2023-12-12 09:48:40 +0100

    Just Mob Heads source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/justmobheads/neoforge/events/NeoForgeHeadDropEvent.java
@@ -0,0 +1,38 @@
+package com.natamus.justmobheads.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.justmobheads.cmds.CommandJmh;
+import com.natamus.justmobheads.events.HeadDropEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeHeadDropEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandJmh.register(e.getDispatcher());
+	}
+
+	@SubscribeEvent
+	public static void mobItemDrop(LivingDropsEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		HeadDropEvent.mobItemDrop(livingEntity.level(), livingEntity, e.getSource());
+	}
+	
+	@SubscribeEvent
+	public static void onPlayerHeadBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		if (!HeadDropEvent.onPlayerHeadBreak(level, e.getPlayer(), e.getPos(), e.getState(), null)) {
+			e.setCanceled(true);
+		}
+	}
+}
