2446bc26eaa4fbee186116e7719087f461765b55
Rick South <rick@natamus.com>
2024-06-27 11:41:28 +0200

    (v8.0) Added autocomplete to the /jmh head command with head names. [+2]

---
+++ Common/src/main/java/com/natamus/justmobheads/functions/JmhCommandFunctions.java
@@ -0,0 +1,15 @@
+package com.natamus.justmobheads.functions;
+
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.natamus.justmobheads.util.HeadData;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.network.chat.Component;
+
+public class JmhCommandFunctions {
+	public static final SuggestionProvider<CommandSourceStack> mobHeadSuggestions = (context, builder) -> SharedSuggestionProvider.suggest(
+		HeadData.headdata.keySet(), builder,
+        value -> value,
+        value -> Component.literal(value)
+    );
+}
