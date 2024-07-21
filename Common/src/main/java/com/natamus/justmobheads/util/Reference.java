8aa3303ea58edca491787dfccfcc5ac6574653d3
Rick South <rick@natamus.com>
2024-07-21 19:50:04 +0200

    (v8.2) Fixed the Slime head being white due to a helmet layer, thanks @James103!

--- Common/src/main/java/com/natamus/justmobheads/util/Reference.java
+++ Common/src/main/java/com/natamus/justmobheads/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.justmobheads.util;
 public class Reference {
 	public static final String MOD_ID = "justmobheads";
 	public static final String NAME = "Just Mob Heads";
-	public static final String VERSION = "8.1";
+	public static final String VERSION = "8.2";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
