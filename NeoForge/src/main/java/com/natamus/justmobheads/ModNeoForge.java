889bb4b223d587a700c72b5dbb9c55e43a193377
Rick South <rick@natamus.com>
2024-10-24 19:52:10 +0200

    Just Mob Heads source code for 1.21.2.

---
+++ NeoForge/src/main/java/com/natamus/justmobheads/ModNeoForge.java
@@ -0,0 +1,39 @@
+package com.natamus.justmobheads;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.justmobheads.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.justmobheads.neoforge.events.NeoForgeHeadDropEvent;
+import com.natamus.justmobheads.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeHeadDropEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
