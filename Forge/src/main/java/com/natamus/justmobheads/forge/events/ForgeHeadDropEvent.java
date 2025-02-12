289bd5ad79aaa2a6b5887a7653a0d9be162a9b96
Rick South <rick@natamus.com>
2025-02-12 23:27:32 +0100

    (v8.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/justmobheads/forge/events/ForgeHeadDropEvent.java
+++ Forge/src/main/java/com/natamus/justmobheads/forge/events/ForgeHeadDropEvent.java
@@ -11,29 +11,27 @@ import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeHeadDropEvent {
     @SubscribeEvent
-    public void registerCommands(RegisterCommandsEvent e) {
+    public static void registerCommands(RegisterCommandsEvent e) {
     	CommandJmh.register(e.getDispatcher());
     }
 
 	@SubscribeEvent
-	public void mobItemDrop(LivingDropsEvent e) {
+	public static void mobItemDrop(LivingDropsEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 		HeadDropEvent.mobItemDrop(livingEntity.level(), livingEntity, e.getSource());
 	}
 	
     @SubscribeEvent
-    public void onItemPickup(EntityItemPickupEvent e) {
+    public static void onItemPickup(EntityItemPickupEvent e) {
         Player player = e.getEntity();
         HeadDropEvent.onItemPickup(player.level(), player, e.getItem().getItem());
     }
 
 	@SubscribeEvent
-	public void onPlayerHeadBreak(BlockEvent.BreakEvent e) {
+	public static void onPlayerHeadBreak(BlockEvent.BreakEvent e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
