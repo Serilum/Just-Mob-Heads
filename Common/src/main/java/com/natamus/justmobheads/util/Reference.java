928f5f26e54f68cdf0bd1e67c905c258e3a1c0f3
Rick South <rick@natamus.com>
2025-10-14 11:35:56 +0200

    (v8.9) (1.21.9/10) Updated the Collective dependency to fix heads dropping twice.

--- Common/src/main/java/com/natamus/justmobheads/util/Reference.java
+++ Common/src/main/java/com/natamus/justmobheads/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.justmobheads.util;
 public class Reference {
 	public static final String MOD_ID = "justmobheads";
 	public static final String NAME = "Just Mob Heads";
-	public static final String VERSION = "8.8";
+	public static final String VERSION = "8.9";
 	public static final String ACCEPTED_VERSIONS = "[1.21.9]";
 }
