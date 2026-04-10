d3452b0b6fc16c592561141a5f66c0c00a4fc742
Rick South <rick@natamus.com>
2026-04-10 02:51:49 +0200

    Just Mob Heads source code for 26.1.2.

---
+++ Forge/src/main/java/com/natamus/justmobheads/forge/events/ForgeHeadDropEvent.java
@@ -0,0 +1,39 @@
+package com.natamus.justmobheads.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.justmobheads.cmds.CommandJmh;
+import com.natamus.justmobheads.events.HeadDropEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeHeadDropEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeHeadDropEvent.class);
+	}
+
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent e) {
+    	CommandJmh.register(e.getDispatcher());
+    }
+
+	@SubscribeEvent
+	public static void mobItemDrop(LivingDropsEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		HeadDropEvent.mobItemDrop(livingEntity.level(), livingEntity, e.getSource());
+	}
+	
+    @SubscribeEvent
+    public static void onItemPickup(EntityItemPickupEvent e) {
+        Player player = e.getEntity();
+        HeadDropEvent.onItemPickup(player.level(), player, e.getItem().getItem());
+    }
+}
