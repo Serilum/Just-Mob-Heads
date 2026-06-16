82a95b2ff321b09cfe6af7050836b139eef702fe
Rick South <rick@natamus.com>
2026-06-17 00:53:34 +0200

    (v9.3) (actually) Fixed translations when only installed on the server for all versions.

--- Common/src/main/java/com/natamus/justmobheads/ModCommon.java
+++ Common/src/main/java/com/natamus/justmobheads/ModCommon.java
@@ -1,7 +1,9 @@
 package com.natamus.justmobheads;
 
+import com.natamus.collective.translations.ServerTranslationPack;
 import com.natamus.justmobheads.config.ConfigHandler;
 import com.natamus.justmobheads.util.HeadData;
+import com.natamus.justmobheads.util.Reference;
 
 public class ModCommon {
 
@@ -12,5 +14,7 @@ public class ModCommon {
 
 	private static void load() {
 		HeadData.init();
+
+		ServerTranslationPack.requireClientTranslations(Reference.NAME);
 	}
 }
